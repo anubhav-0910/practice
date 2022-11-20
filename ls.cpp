0a300a46c6708b6f5ae0a9e5be10647651e92bbf
msriramsai23 <m.sriramsai23@gmail.com>
2022-11-21 01:44:56 +0530

    long long long vector

--- ls.cpp
+++ ls.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int linear_search(vector<int>v,int x){
-	for(int i=0;i<5;i++){
+	for(int i=0;i<v.size();i++){
 		if(v[i]==x) return i;
 	}
 return -1;
@@ -11,7 +11,12 @@ return -1;
 	
 	
 int main(){
-vector<int>v={1,4,7,2,8};
+int n;
+cin>>n;
+vector<int>v;
+for(int i=0;i<n;i++){
+v[i]=i+5;
+}
 int x;
 cin>>x;
 return linear_search(v,x);
